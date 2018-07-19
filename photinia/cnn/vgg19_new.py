e7f1f2418e1e9f29f98bc37db398e76922ebb167
little1tow <zhkun@mail.ustc.edu.cn>
2018-07-19 10:08:59 +0800

    debug and remove
    fix some bugs and remove to cnn folder

--- photinia/cnn/vgg19_new.py
+++ photinia/cnn/vgg19_new.py
@@ -189,6 +189,18 @@ class Vgg19(ph.Widget):
             w_init=ph.init.RandomNormal(stddev=1e-4)
         )
 
+    @property
+	def fc6(self):
+    	return self._fc6
+
+    @property
+    def fc7(self):
+        return self._fc7
+
+    @property
+    def fc8(self):
+        return self._fc8
+
     def _setup(self, x):
         # Convert RGB to BGR
         red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=x)
@@ -233,10 +245,13 @@ class Vgg19(ph.Widget):
     def load_pretrain(self, model_file='vgg19'):
         ph.io.load_model_from_tree(self, model_file)
 
-    def load_parameters(self, model_file='vgg19.npy'):
+    def load_parameters(self, prepath=None, model_file='vgg19.npy'):
         self.data_dict = np.load(model_file, encoding='latin1').item()
         param_dict = {}
-        prepath = 'vgg16'
+        if prepath is None:
+        	prepath = 'vgg19'
+        else:
+        	prepath = prepath + '/vgg16'
 
         for op_name in self.data_dict:
             for param in self.data_dict[op_name]:
