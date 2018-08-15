8144bcd872cbc7218aa28dbafed6dc025a963f6e
XoriieInpottn <XoriieInpottn@126.com>
2018-08-15 02:36:56 -0500

    Add AccCalculator and BiClassCalculator. They have not been tested.

--- photinia/train.py
+++ photinia/train.py
@@ -8,7 +8,74 @@
 import collections
 import datetime as dt
 
+import numpy as np
+
 from . import ops
+
+
+class AccCalculator(object):
+
+    def __init__(self):
+        self._num_hit = 0
+        self._num_all = 0
+
+    def update(self, label_pred, label_true):
+        hit = np.equal(label_pred, label_true)
+        hit = np.float32(hit)
+        self._num_hit += float(np.sum(hit))
+        self._num_all += len(hit)
+
+    def reset(self):
+        self._num_hit = 0
+        self._num_all = 0
+
+    @property
+    def accuracy(self):
+        return self._num_hit / self._num_all if self._num_all > 0 else 0.0
+
+
+class BiClassCalculator(object):
+
+    def __init__(self):
+        self._tp = 0
+        self._tn = 0
+        self._fp = 0
+        self._fn = 0
+
+    def update(self, label_predict, label_true):
+        hit = np.equal(label_predict, label_true)
+        hit = np.float32(hit)
+        miss = 1.0 - hit
+
+        pos = np.float32(label_predict)
+        neg = 1.0 - pos
+
+        self._tp += np.sum(hit * pos, keepdims=False)
+        self._tn += np.sum(hit * neg, keepdims=False)
+        self._fp += np.sum(miss * pos, keepdims=False)
+        self._fn += np.sum(miss * neg, keepdims=False)
+
+    @property
+    def precision(self):
+        num_pos_pred = self._tp + self._fp
+        return self._tp / num_pos_pred if num_pos_pred > 0 else 0.0
+
+    @property
+    def recall(self):
+        num_pos_true = self._tp + self._fn
+        return self._tp / num_pos_true if num_pos_true > 0 else 0.0
+
+    @property
+    def f1(self):
+        pre = self.precision
+        rec = self.recall
+        return 2 * (pre * rec) / (pre + rec)
+
+    @property
+    def accuracy(self):
+        num_hit = self._tp + self._tn
+        num_all = self._tp + self._tn + self._fp + self._fn
+        return num_hit / num_all if num_all > 0 else 0.0
 
 
 def call_for_batch(context, slot, data_source):
@@ -68,40 +135,6 @@ def call_for_all(context, slot, data_source):
         else:
             # Should not be reached, since Slot ALWAYS returns tuple or dict.
             raise RuntimeError('Invalid Slot outputs type.')
-    context.update(ret)
-    return ret
-
-
-def calculate_performance(context, tp='tp', tn='tn', fp='fp', fn='fn'):
-    """
-
-    Args:
-        context (dict):
-        tp (str):
-        tn (str):
-        fp (str):
-        fn (str):
-
-    Returns:
-        dict:
-
-    """
-    tp = sum(context[tp])
-    tn = sum(context[tn])
-    fp = sum(context[fp])
-    fn = sum(context[fn])
-
-    pre = tp / (tp + fp)
-    rec = tp / (tp + fn)
-    f1 = 2 * (pre * rec) / (pre + rec)
-    acc = (tp + tn) / (tp + tn + fp + fn)
-
-    ret = dict(
-        pre=pre,
-        rec=rec,
-        f1=f1,
-        acc=acc
-    )
     context.update(ret)
     return ret
 
