5b336578ff37e50debb787092f43701baaf21fb1
XoriieInpottn <XoriieInpottn@126.com>
2018-08-31 17:01:31 -0500

    Add DNN module.

---
+++ photinia/dnn/resnet.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+@author: xi
+@since: 2018-08-31
+"""
+
+import photinia as ph
+
+
+class DeepResNet(ph.Widget):
+
+    def __init__(self,
+                 name,
+                 input_size,
+                 output_size,
+                 hidden_size,
+                 num_layers=1):
+        self._input_size = input_size
+        self._output_size = output_size
+        self._hidden_size = hidden_size
+        self._num_layers = num_layers
+        super(DeepResNet, self).__init__(name)
+
+    def _build(self):
+        self._input_layer = ph.Linear('input_layer', self._input_size, self._hidden_size)
+
+        res_layers = self._res_layers = list()
+        for i in range(self._num_layers):
+            res_layer = ph.ResidualLinear(f'res_{str(i)}', self._hidden_size)
+            res_layers.append(res_layer)
+
+        self._output_layer = ph.Linear('output_layer', self._hidden_size, self._output_size)
+
+    def _setup(self, input_x, activation=ph.ops.lrelu, name='out'):
+        h = self._input_layer.setup(input_x)
+        if activation:
+            h = activation(h)
+
+        for layer in self._res_layers:
+            h = layer.setup(h, activation=activation)
+            if activation:
+                h = activation(h)
+
+        y = self._output_layer.setup(h, name=name)
+        return y
