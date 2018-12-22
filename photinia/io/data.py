1860f2b779ba8236208b26f73b83066995b36c7c
XoriieInpottn <XoriieInpottn@126.com>
2018-12-22 17:37:02 +0800

    Changes in photinia.io.data. 1) Change data item type from "tuple" to "namedtuple". 2) Change the stop behavior of all data source. In the previous version, when a data source is exhausted, the next() call returns None and __next__() raises StopIteration. In the new version, both of them raise exception.

--- photinia/io/data.py
+++ photinia/io/data.py
@@ -16,11 +16,14 @@ import numpy as np
 
 
 class DataSource(object):
-    """DataSource
-    """
 
-    def meta(self):
-        raise NotImplementedError()
+    def __init__(self, field_names):
+        self._field_names = list(field_names)
+        self._data_model = collections.namedtuple('DataItem', field_names)
+
+    @property
+    def field_names(self):
+        return self._field_names
 
     def next(self):
         raise NotImplementedError()
@@ -29,38 +32,30 @@ class DataSource(object):
         return self
 
     def __next__(self):
-        sample = self.next()
-        if sample is None:
-            raise StopIteration()
-        return sample
+        return self.next()
 
 
 class MemorySource(DataSource):
 
     def __init__(self,
-                 columns,
-                 column_names,
+                 field_names,
+                 docs,
                  dtype=None):
-        """Data source from memory.
+        super(MemorySource, self).__init__(field_names)
 
-        Args:
-            columns: Tuple of list, np.array or any iterable objects.
-            column_names (tuple|list): Column names.
-            dtype: Data type.
-        """
-        self._num_comp = len(columns)
+        self._columns = [list() for _ in range(len(self._field_names))]
+        for doc in docs:
+            for i, field_name in enumerate(self._field_names):
+                self._columns[i].append(doc[field_name])
+        self._columns = [
+            np.array(column, dtype=dtype)
+            for column in self._columns
+        ]
+
+        self._num_comp = len(self._field_names)
         if self._num_comp == 0:
             raise ValueError('At least 1 data object should be given.')
-        self._meta = tuple(str(column_name) for column_name in column_names)
-        self._columns = [np.array(column, dtype=dtype) for column in columns]
-        size = None
-        for column in self._columns:
-            if size is None:
-                size = len(column)
-                continue
-            if len(column) != size:
-                raise ValueError('All data components must have the same size.')
-        self._size = size
+        self._size = len(self._columns[0])
         self._start = 0
         self._loop = 0
 
@@ -76,18 +71,15 @@ class MemorySource(DataSource):
     def loop(self):
         return self._loop
 
-    def meta(self):
-        return self._meta
-
     def next(self):
         if self._start >= self._size:
             self._start = 0
             self._loop += 1
             self.shuffle()
-            return None
-        row = tuple(
-            column[self._start]
-            for column in self._columns
+            raise StopIteration()
+        row = self._data_model(
+            *(column[self._start]
+              for column in self._columns)
         )
         self._start += 1
         return row
@@ -99,7 +91,10 @@ class MemorySource(DataSource):
         real_size = len(batch[0])
         while real_size < size:
             batch1 = self._next_batch(size - real_size)
-            batch = tuple(np.concatenate((batch[i], batch1[i]), 0) for i in range(self._num_comp))
+            batch = self._data_model(
+                *(np.concatenate((batch[i], batch1[i]), 0)
+                  for i in range(self._num_comp))
+            )
             real_size = len(batch[0])
         return batch
 
@@ -110,10 +105,16 @@ class MemorySource(DataSource):
             self.shuffle()
         end = self._start + size
         if end < self._size:
-            batch = tuple(self._columns[i][self._start:end].copy() for i in range(self._num_comp))
+            batch = self._data_model(
+                *(self._columns[i][self._start:end].copy()
+                  for i in range(self._num_comp))
+            )
             self._start += size
         else:
-            batch = tuple(self._columns[i][self._start:].copy() for i in range(self._num_comp))
+            batch = self._data_model(
+                *(self._columns[i][self._start:].copy()
+                  for i in range(self._num_comp))
+            )
             self._start = 0
             self._loop += 1
         return batch
@@ -132,34 +133,29 @@ class MemorySource(DataSource):
 
 class CSVSource(DataSource):
 
-    def __init__(self, fp, column_names, delimiter=','):
-        self._meta = tuple(column_name for column_name in column_names)
+    def __init__(self, field_names, fp, delimiter=','):
+        super(CSVSource, self).__init__(field_names)
 
         reader = csv.DictReader(fp, delimiter=delimiter)
         self._iter = iter(reader)
-        self._columns = [list() for _ in self._meta]
+        self._docs = list()
 
         self._memory_source = None
-
-    def meta(self):
-        return self._meta
 
     def next(self):
         if self._memory_source is None:
             try:
                 doc = next(self._iter)
             except StopIteration:
-                self._memory_source = MemorySource(self._columns, self._meta)
+                self._memory_source = MemorySource(self._field_names, self._docs)
                 self._iter = None
-                self._columns = None
-                return None
-            for i, column_name in enumerate(self._meta):
-                cell = doc[column_name]
-                self._columns[i].append(cell)
+                self._docs = None
+                raise StopIteration()
+            self._docs.append(doc)
             # print('DEBUG: Fetch from file.')
-            return tuple(
-                column[-1]
-                for column in self._columns
+            return self._data_model(
+                *(doc[field_name]
+                  for field_name in self._field_names)
             )
         # print('DEBUG: Fetch from memory.')
         return self._memory_source.next()
@@ -168,8 +164,8 @@ class CSVSource(DataSource):
 class MongoSource(DataSource):
 
     def __init__(self,
+                 field_names,
                  coll,
-                 column_names,
                  filters,
                  random_order,
                  buffer_size=100000):
@@ -177,7 +173,6 @@ class MongoSource(DataSource):
 
         Args:
             coll: MongoDB collection object.
-            column_names (list[str]|tuple[str]): Column names that will query from the database.
             filters (dict): Filters which will be pass to MongoDB's find() operation.
             random_order (bool): If iterate the collections in random order.
                 This is usually set to True when used as train set.
@@ -185,54 +180,59 @@ class MongoSource(DataSource):
                 This option will only take effect when random_order is True.
 
         """
-        super(MongoSource, self).__init__()
+        super(MongoSource, self).__init__(field_names)
         self._coll = coll
-        self._random_order = random_order
-        self._meta = tuple(column_name for column_name in column_names)
-        self._projections = {column_name: 1 for column_name in column_names}
         self._filters = filters if filters is not None else {}
+        self._projections = {field_name: 1 for field_name in field_names}
+        self._random_order = random_order
         self._buffer_size = buffer_size
 
         self._cursor = None
         self._buffer = list()
-
-    def meta(self):
-        return self._meta
 
     def next(self):
         if self._random_order:
             doc = self._random_next()
         else:
             doc = self._normal_order()
-        return tuple(
-            doc[column_name]
-            for column_name in self._meta
-        ) if doc is not None else None
+        return self._data_model(
+            *(doc[field_name]
+              for field_name in self._field_names)
+        )
 
     def _random_next(self):
+        #
+        # fetch next ID from the database
         _id = None
         error = None
         for _ in range(3):
             try:
                 _id = self._next_id()
                 break
-            except StopIteration:
-                return None
+            except StopIteration as e:
+                raise e
             except Exception as e:
                 error = e
                 time.sleep(3)
                 continue
+
+        #
+        # add the ID from the buffer
         if _id is None:
             raise error
-
         if len(self._buffer) < self._buffer_size:
             self._buffer.append(_id)
         else:
             index = random.randint(0, self._buffer_size - 1)
             self._buffer[index] = _id
+
+        #
+        # get an ID from buffer randomly
         index = random.randint(0, len(self._buffer) - 1)
         _id = self._buffer[index]
 
+        #
+        # get the doc based on the ID
         doc = None
         error = None
         for _ in range(3):
@@ -253,10 +253,11 @@ class MongoSource(DataSource):
             self._cursor = self._coll.find(self._filters, {'_id': 1})
         try:
             doc = next(self._cursor)
-        except StopIteration as e:
-            self._cursor = None
-            raise e
         except Exception as e:
+            #
+            # the exception may be:
+            # 1) StopIteration
+            # 2) CursorTimeout
             self._cursor = None
             raise e
         return doc['_id']
@@ -265,22 +266,18 @@ class MongoSource(DataSource):
         doc = None
         error = None
         for _ in range(3):
+            if self._cursor is None:
+                self._cursor = self._coll.find(self._filters, self._projections)
             try:
-                if self._cursor is None:
-                    self._cursor = self._coll.find(self._filters, self._projections)
-                try:
-                    doc = next(self._cursor)
-                except StopIteration:
-                    self._cursor = None
-                    return None
-                except Exception as e:
-                    self._cursor = None
-                    raise e
-                break
+                doc = next(self._cursor)
+            except StopIteration as e:
+                raise e
             except Exception as e:
+                self._cursor = None
                 error = e
                 time.sleep(3)
                 continue
+            break
         if doc is None:
             raise error
 
@@ -289,18 +286,19 @@ class MongoSource(DataSource):
 
 class BatchSource(DataSource):
 
-    def __init__(self, input_source, batch_size):
-        """
+    def __init__(self,
+                 input_source,
+                 batch_size):
+        """Return data item in batch.
 
         Args:
-            input_source (DataSource):
-            batch_size (int):
+            input_source (DataSource): Data source to be wrapped.
+            batch_size (int): Batch size.
 
         """
         self._input_source = input_source
+        super(BatchSource, self).__init__(input_source.field_names)
         self._batch_size = batch_size
-
-        self._meta = self._input_source.meta()
 
         self._cell_fns = collections.defaultdict(collections.deque)
         self._column_fns = collections.defaultdict(collections.deque)
@@ -311,117 +309,101 @@ class BatchSource(DataSource):
     def batch_size(self):
         return self._batch_size
 
-    def add_cell_fns(self, column_name, fns):
+    def add_cell_fns(self, field_name, fns):
         if callable(fns):
             fns = [fns]
         elif not isinstance(fns, (list, tuple)):
             raise ValueError('fns should be callable or list(tuple) of callables.')
-        if type(column_name) is not list:
-            column_name = [column_name]
-        for item in column_name:
+        if type(field_name) is not list:
+            field_name = [field_name]
+        for item in field_name:
             self._cell_fns[item] += fns
 
-    def add_column_fns(self, column_name, fns):
+    def add_column_fns(self, field_name, fns):
         if callable(fns):
             fns = [fns]
         elif not isinstance(fns, (list, tuple)):
             raise ValueError('fns should be callable or list(tuple) of callables.')
-        if type(column_name) is not list:
-            column_name = [column_name]
-        for item in column_name:
+        if type(field_name) is not list:
+            field_name = [field_name]
+        for item in field_name:
             self._column_fns[item] += fns
 
-    def meta(self):
-        return self._meta
-
     def next(self):
-        if self._batch_size:
-            if self._eof:
-                self._eof = False
-                return None
-            columns = tuple(list() for _ in self._meta)
-            for i in range(self._batch_size):
-                row = self._next_one()
-                if isinstance(row, Exception):
-                    raise row
-                if row is None:
-                    if i == 0:
-                        return None
-                    else:
-                        self._eof = True
-                        break
-                for j, cell in enumerate(row):
-                    columns[j].append(cell)
-            columns = tuple(
-                self._apply_batch_mappers(column_name, column)
-                for column_name, column in zip(self._meta, columns)
-            )
-            return columns
-        else:
-            return self._next_one()
+        if self._eof:
+            self._eof = False
+            raise StopIteration()
+
+        batch_doc = tuple(
+            list() for _ in self._field_names
+        )
+        for i in range(self._batch_size):
+            try:
+                doc = self._next_one()
+            except StopIteration as e:
+                if i == 0:
+                    raise e
+                else:
+                    self._eof = True
+                    break
+            for j, value in enumerate(doc):
+                batch_doc[j].append(value)
+
+        batch_doc = self._data_model(
+            *(self._apply_column_fns(field_name, value)
+              for field_name, value in zip(self._field_names, batch_doc))
+        )
+        return batch_doc
 
     def _next_one(self):
-        row = self._input_source.next()
-        if row is None:
-            return None
-        row = tuple(
-            self._apply_column_mappers(row[i], column_name)
-            for i, column_name in enumerate(self._meta)
+        doc = self._input_source.next()
+        doc = tuple(
+            self._apply_cell_fns(field_name, value)
+            for field_name, value in zip(self._field_names, doc)
         )
-        return row
+        return doc
 
-    def _apply_column_mappers(self, cell, column_name):
-        if column_name in self._cell_fns:
-            for fn in self._cell_fns[column_name]:
-                cell = fn(cell)
-        return cell
+    def _apply_cell_fns(self, field_name, value):
+        if field_name in self._cell_fns:
+            for fn in self._cell_fns[field_name]:
+                value = fn(value)
+        return value
 
-    def _apply_batch_mappers(self, column_name, column):
-        if column_name in self._column_fns:
-            for fn in self._column_fns[column_name]:
+    def _apply_column_fns(self, field_name, column):
+        if field_name in self._column_fns:
+            for fn in self._column_fns[field_name]:
                 column = fn(column)
         return column
 
 
 class ThreadBufferedSource(DataSource):
 
-    def __init__(self, input_source, buffer_size=1000):
+    def __init__(self,
+                 input_source,
+                 buffer_size=1000):
+        """Preload data to a buffer in another thread.
+
+        Args:
+            input_source (DataSource): Data source to be wrapped.
+            buffer_size (int): Buffer size.
+
+        """
         self._input_source = input_source
+        super(ThreadBufferedSource, self).__init__(input_source.field_names)
         if isinstance(buffer_size, int) and buffer_size > 0:
             self._buffer_size = buffer_size
         else:
-            raise ValueError('Argument buffer_size should be a positive integer.')
+            raise ValueError('buffer_size should be a positive integer.')
         #
         # Async Loading
-        # self._main_thread = threading.current_thread()
         self._queue = queue.Queue(buffer_size)
-        # self._load_threshold = self._buffer_size // 3
         self._thread = None
-        # self._thread_lock = threading.Semaphore(1)
-
-    def meta(self):
-        return self._input_source.meta()
 
     def next(self):
         if self._thread is None:
             self._thread = threading.Thread(target=self._load)
             self._thread.setDaemon(True)
             self._thread.start()
-        # if (self._queue.qsize() <= self._load_threshold
-        #         and (self._thread is None or not self._thread.is_alive())):
-        #     self._thread = threading.Thread(target=self._load)
-        #     self._thread.setDaemon(True)
-        #     self._thread.start()
-        # while True:
-        #     try:
-        #         row = self._queue.get(block=True)
-        #     except queue.Empty:
-        #         if self._thread is None or not self._thread.is_alive():
-        #             self._thread = threading.Thread(target=self._load)
-        #             self._thread.setDaemon(True)
-        #             self._thread.start()
-        #         continue
-        #     break
         row = self._queue.get(block=True)
         if isinstance(row, Exception):
             raise row
@@ -431,7 +413,6 @@ class ThreadBufferedSource(DataSource):
         """This method is executed in another thread!
         """
         # print('DEBUG: Loading thread started.')
-        # for i in range(self._buffer_size):
         while True:
             try:
                 row = self._input_source.next()
@@ -439,6 +420,4 @@ class ThreadBufferedSource(DataSource):
                 self._queue.put(e)
                 break
             self._queue.put(row, block=True)
-        # if not self._main_thread.is_alive():
-        #     break
         # print('DEBUG: Loading thread stopped. %d loaded' % (i + 1))
