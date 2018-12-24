3ab98b5ec81a982d65a84424fe9c9b760044bfdc
XoriieInpottn <XoriieInpottn@126.com>
2018-12-24 17:03:49 +0800

    Complete Application class, but still not usable.

--- photinia/core/application.py
+++ photinia/core/application.py
@@ -6,6 +6,7 @@
 """
 
 import threading
+import code
 
 
 class Application(object):
@@ -16,11 +17,24 @@ class Application(object):
         self._app_thread.setDaemon(True)
         self._ret_code = -1
 
+        self._interrupt_lock = threading.Semaphore(1)
+        self._interrupt = False
+
+        self._wait_for_interrupt = threading.Semaphore(0)
+        self._wait_for_continue = threading.Semaphore(0)
+
     def _main(self):
         self._ret_code = self.main()
 
     def main(self):
         raise NotImplementedError()
+
+    def checkpoint(self):
+        with self._interrupt_lock:
+            interrupt = self._interrupt
+        if interrupt:
+            self._wait_for_interrupt.release()
+            self._wait_for_continue.acquire()
 
     def run(self):
         self._app_thread.start()
@@ -28,10 +42,29 @@ class Application(object):
             try:
                 self._app_thread.join()
             except KeyboardInterrupt:
+                with self._interrupt_lock:
+                    self._interrupt = True
+                print('\nWaiting for the checkpoint to interrupt...')
+                self._wait_for_interrupt.acquire()
+
                 if self._shell():
                     break
+
+                with self._interrupt_lock:
+                    self._interrupt = False
+                self._wait_for_continue.release()
         return self._ret_code
 
     def _shell(self):
-        if input('Exit?').lower() == 'y':
-            return True
+        code.interact(
+            banner='\nYour application is interrupted.',
+            local={
+                'help': self.help
+            },
+            exitmsg='\nYour application will continue to run.'
+        )
+
+    def help(self):
+        print('This is help page.')
+        print('This is help page.')
+        print('This is help page.')
