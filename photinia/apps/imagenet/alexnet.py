eba5ba2fdd46deb25b8dd04d821dc080bd907e89
XoriieInpottn <XoriieInpottn@126.com>
2019-03-19 15:02:18 +0800

    Polish the code.

--- photinia/apps/imagenet/alexnet.py
+++ photinia/apps/imagenet/alexnet.py
@@ -5,108 +5,122 @@
 @since: 2018-03-03
 """
 
+import numpy as np
 import tensorflow as tf
 
 import photinia as ph
+
+HEIGHT = 227
+WIDTH = 227
+SIZE = (HEIGHT, WIDTH)
+
+MEAN = [103.939, 116.779, 123.68]
 
 
 class AlexNet(ph.Widget):
 
     def __init__(self, name='alexnet'):
-        self._height = 227
-        self._width = 227
+        self._height = HEIGHT
+        self._width = WIDTH
+        self._mean = np.reshape(MEAN, (1, 1, 1, 3))
         super(AlexNet, self).__init__(name)
 
     def _build(self):
-        #
-        # conv1 padding=VALID
-        self._conv1 = ph.Conv2D(
-            'conv1',
+        ###############################################################################################################
+        # -> (55, 55, 96)
+        # -> (27, 27, 96)
+        ###############################################################################################################
+        self._conv_1 = ph.Conv2D(
+            'conv_1',
             input_size=[self._height, self._width, 3],
             output_channels=96,
             filter_height=11, filter_width=11, stride_width=4, stride_height=4,
             padding='VALID'
         )
-        self._pool1 = ph.Pool2D(
-            'pool1',
-            input_size=self._conv1.output_size,
+        self._pool_1 = ph.Pool2D(
+            'pool_1',
+            input_size=self._conv_1.output_size,
             filter_height=3, filter_width=3, stride_height=2, stride_width=2,
             padding='VALID',
             pool_type='max'
         )
-        #
-        # conv2, 这里是拆分训练的
-        self._conv2 = ph.GroupConv2D(
-            'conv2',
-            input_size=self._pool1.output_size,
+        ###############################################################################################################
+        # -> (27, 27, 256)
+        # -> (13, 13, 256)
+        ###############################################################################################################
+        self._conv_2 = ph.GroupConv2D(
+            'conv_2',
+            input_size=self._pool_1.output_size,
             output_channels=256,
             num_groups=2,
-            filter_height=5, filter_width=5, stride_height=1, stride_width=1
+            filter_height=5, filter_width=5, stride_height=1, stride_width=1,
+            padding='SAME'
         )
-        self._pool2 = ph.Pool2D(
-            'pool2',
-            input_size=self._conv2.output_size,
+        self._pool_2 = ph.Pool2D(
+            'pool_2',
+            input_size=self._conv_2.output_size,
             filter_height=3, filter_width=3, stride_height=2, stride_width=2,
-            padding='VALID',
-            pool_type='max'
+            padding='VALID', pool_type='max'
         )
-        #
-        # conv3
-        self._conv3 = ph.Conv2D(
-            'conv3',
-            input_size=self._pool2.output_size,
+        ###############################################################################################################
+        # -> (13, 13, 384)
+        ###############################################################################################################
+        self._conv_3 = ph.Conv2D(
+            'conv_3',
+            input_size=self._pool_2.output_size,
             output_channels=384,
-            filter_width=3, filter_height=3, stride_width=1, stride_height=1
+            filter_width=3, filter_height=3, stride_width=1, stride_height=1,
+            padding='SAME'
         )
-        #
-        # conv4, 这里是拆分训练的
-        self._conv4 = ph.GroupConv2D(
-            'conv4',
-            input_size=self._conv3.output_size,
+        ###############################################################################################################
+        # -> (13, 13, 384)
+        ###############################################################################################################
+        self._conv_4 = ph.GroupConv2D(
+            'conv_4',
+            input_size=self._conv_3.output_size,
             output_channels=384,
             num_groups=2,
-            filter_width=3, filter_height=3, stride_width=1, stride_height=1
+            filter_width=3, filter_height=3, stride_width=1, stride_height=1,
+            padding='SAME'
         )
-        #
-        # conv5, 这里是拆分训练的
-        self._conv5 = ph.GroupConv2D(
-            'conv5',
-            input_size=self._conv4.output_size,
+        ###############################################################################################################
+        # -> (13, 13, 256)
+        # -> (6, 6, 256)
+        ###############################################################################################################
+        self._conv_5 = ph.GroupConv2D(
+            'conv_5',
+            input_size=self._conv_4.output_size,
             output_channels=256,
             num_groups=2,
-            filter_width=3, filter_height=3, stride_width=1, stride_height=1
+            filter_width=3, filter_height=3, stride_width=1, stride_height=1,
+            padding='SAME'
         )
-        self._pool5 = ph.Pool2D(
-            'pool5',
-            input_size=self._conv5.output_size,
+        self._pool_5 = ph.Pool2D(
+            'pool_5',
+            input_size=self._conv_5.output_size,
             filter_height=3, filter_width=3, stride_height=2, stride_width=2,
             padding='VALID', pool_type='max'
         )
         #
         # fc layer
-        self._fc6 = ph.Linear('fc6', input_size=self._pool5.flat_size, output_size=4096)
-        self._fc7 = ph.Linear('fc7', input_size=self._fc6.output_size, output_size=4096)
-        self._fc8 = ph.Linear(
-            'fc8',
-            input_size=self._fc7.output_size, output_size=1000,
-            w_init=ph.init.RandomNormal(stddev=1e-4)
-        )
+        self._dense_6 = ph.Linear('dense_6', input_size=self._pool_5.flat_size, output_size=4096)
+        self._dense_7 = ph.Linear('dense_7', input_size=self._dense_6.output_size, output_size=4096)
+        self._dense_8 = ph.Linear('dense_8', input_size=self._dense_7.output_size, output_size=1000)
 
     def _setup(self, x):
-        h = ph.setup(
-            x,
-            [self._conv1, tf.nn.relu, self._lrn, self._pool1,
-             self._conv2, tf.nn.relu, self._lrn, self._pool2,
-             self._conv3, tf.nn.relu,
-             self._conv4, tf.nn.relu,
-             self._conv5, tf.nn.relu, self._pool5,
-             ph.ops.flatten,
-             self._fc6, tf.nn.relu,
-             self._fc7, tf.nn.relu]
+        return ph.setup(
+            x - self._mean, [
+                self._conv_1, tf.nn.relu, self._lrn, self._pool_1, 'map_1',
+                self._conv_2, tf.nn.relu, self._lrn, self._pool_2, 'map_2',
+                self._conv_3, tf.nn.relu, 'map_3',
+                self._conv_4, tf.nn.relu, 'map_4',
+                self._conv_5, tf.nn.relu, self._pool_5, 'map_5',
+                ph.ops.flatten, 'feature_5',
+                self._dense_6, tf.nn.relu, 'feature_6',
+                self._dense_7, tf.nn.relu, 'feature_7',
+                self._dense_8, tf.nn.softmax
+            ]
         )
-        y = self._fc8.setup(h)
-        y = tf.nn.softmax(y)
-        return y, h
 
     @staticmethod
     def _lrn(x):
@@ -117,6 +131,3 @@ class AlexNet(ph.Widget):
             beta=0.75,
             bias=1.0
         )
-
-    def load_pretrain(self, model_file='alexnet.pickle'):
-        ph.io.load_model_from_file(self, model_file)
