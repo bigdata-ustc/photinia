7ea24758b4e477f582fd63eddbb3f63f370d6275
XoriieInpottn <XoriieInpottn@126.com>
2019-03-23 18:09:22 +0800

    Allow ph.apps.imagenet.AlexNet to use custom activation functions.

--- photinia/apps/imagenet/alexnet.py
+++ photinia/apps/imagenet/alexnet.py
@@ -19,17 +19,30 @@ MEAN = [103.939, 116.779, 123.68]
 
 class AlexNet(ph.Widget):
 
-    def __init__(self, name):
+    def __init__(self,
+                 name,
+                 activation=tf.nn.relu):
+        """AlexNet (Krizhevsky, Alex, Ilya Sutskever, and Geoffrey E. Hinton. "Imagenet classification with deep
+        convolutional neural networks." In Advances in neural information processing systems, pp. 1097-1105. 2012.)
+
+        Args:
+            name (str): The widget name.
+            activation: The activation function.
+                Note that the original activation function is ReLU. If you change the activation function when using
+                the pre-trained parameters, the model cannot guarantee the original performance. Take your own risk!
+
+        """
         self._height = HEIGHT
         self._width = WIDTH
         self._mean = np.reshape(MEAN, (1, 1, 1, 3))
+        self._activation = activation
         super(AlexNet, self).__init__(name)
 
     def _build(self):
-        ###############################################################################################################
+        ################################################################################
         # -> (55, 55, 96)
         # -> (27, 27, 96)
-        ###############################################################################################################
+        ################################################################################
         self._conv_1 = ph.Conv2D(
             'conv_1',
             input_size=[self._height, self._width, 3],
@@ -44,10 +57,10 @@ class AlexNet(ph.Widget):
             padding='VALID',
             pool_type='max'
         )
-        ###############################################################################################################
+        ################################################################################
         # -> (27, 27, 256)
         # -> (13, 13, 256)
-        ###############################################################################################################
+        ################################################################################
         self._conv_2 = ph.GroupConv2D(
             'conv_2',
             input_size=self._pool_1.output_size,
@@ -62,9 +75,9 @@ class AlexNet(ph.Widget):
             filter_height=3, filter_width=3, stride_height=2, stride_width=2,
             padding='VALID', pool_type='max'
         )
-        ###############################################################################################################
+        ################################################################################
         # -> (13, 13, 384)
-        ###############################################################################################################
+        ################################################################################
         self._conv_3 = ph.Conv2D(
             'conv_3',
             input_size=self._pool_2.output_size,
@@ -72,9 +85,9 @@ class AlexNet(ph.Widget):
             filter_width=3, filter_height=3, stride_width=1, stride_height=1,
             padding='SAME'
         )
-        ###############################################################################################################
+        ################################################################################
         # -> (13, 13, 384)
-        ###############################################################################################################
+        ################################################################################
         self._conv_4 = ph.GroupConv2D(
             'conv_4',
             input_size=self._conv_3.output_size,
@@ -83,10 +96,10 @@ class AlexNet(ph.Widget):
             filter_width=3, filter_height=3, stride_width=1, stride_height=1,
             padding='SAME'
         )
-        ###############################################################################################################
+        ################################################################################
         # -> (13, 13, 256)
         # -> (6, 6, 256)
-        ###############################################################################################################
+        ################################################################################
         self._conv_5 = ph.GroupConv2D(
             'conv_5',
             input_size=self._conv_4.output_size,
@@ -110,14 +123,14 @@ class AlexNet(ph.Widget):
     def _setup(self, x):
         return ph.setup(
             x - self._mean, [
-                self._conv_1, tf.nn.relu, self._lrn, self._pool_1, 'map_1',
-                self._conv_2, tf.nn.relu, self._lrn, self._pool_2, 'map_2',
-                self._conv_3, tf.nn.relu, 'map_3',
-                self._conv_4, tf.nn.relu, 'map_4',
-                self._conv_5, tf.nn.relu, self._pool_5, 'map_5',
+                self._conv_1, self._activation, self._lrn, self._pool_1, 'map_1',
+                self._conv_2, self._activation, self._lrn, self._pool_2, 'map_2',
+                self._conv_3, self._activation, 'map_3',
+                self._conv_4, self._activation, 'map_4',
+                self._conv_5, self._activation, self._pool_5, 'map_5',
                 ph.ops.flatten, 'feature_5',
-                self._dense_6, tf.nn.relu, 'feature_6',
-                self._dense_7, tf.nn.relu, 'feature_7',
+                self._dense_6, self._activation, 'feature_6',
+                self._dense_7, self._activation, 'feature_7',
                 self._dense_8, tf.nn.softmax
             ]
         )
